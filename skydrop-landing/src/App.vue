4b1b74f43395cd9f9aa5e2b7d760c2e4aef97b14
DezkaReid <elmaildeldezkareid@gmail.com>
2017-06-08 01:01:56 -0500

    Added component menu

--- skydrop-landing/src/App.vue
+++ skydrop-landing/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <MenuSkydrop></MenuSkydrop>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import MenuSkydrop from './components/MenuSkydrop'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    MenuSkydrop
   }
 }
 </script>
@@ -21,8 +20,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ skydrop-landing/src/components/MenuSkydrop.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-nav>
+    <ul>
+      <li>
+        <a href="#!">
+          <img src="http://www.skydrop.com.mx/assets/img/logo_header@2x-96e1c4677177e38c484b5736e1a62cb1.png"/>
+        </a>
+      </li>
+      <li>
+        <a href="#!">Mensajería</a>
+      </li>
+      <li>
+        <a href="#!">E-commerce</a>
+      </li>
+      <li>
+        <a href="#!">Restaurantes</a>
+      </li>
+    </ul>
+    <ul class="right">
+      <li>
+        <a href="#!">Contacto</a>
+      </li>
+      <li>
+        <a href="#!">Iniciar Sesión</a>
+      </li>
+      <li>
+        <a href="#!">CONVIÉRTETE EN REPARTIDOR</a>
+      </li>
+    </ul>
+    <v-side-nav id="side"></v-side-nav>
+  </v-nav>
+</template>
+
+<script>
+  export default {
+    name: 'menuskydrop'
+  }
+</script>
+
+<style scoped>
+  img {
+    max-width: 148px;
+    vertical-align: middle;
+  }
+</style>
