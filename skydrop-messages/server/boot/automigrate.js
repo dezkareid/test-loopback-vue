e808ef03573cd4e6a7f3ec29a0cb4f3f22bfa79b
DezkaReid <elmaildeldezkareid@gmail.com>
2017-06-10 07:43:38 -0500

    lint changes

--- skydrop-messages/server/boot/automigrate.js
+++ skydrop-messages/server/boot/automigrate.js
@@ -1,6 +1,8 @@
-module.exports = function (server) {
-  var ds = server.dataSources.postgresDB
-  ds.automigrate(null, function (err) {
+'use strict';
+
+module.exports = function(server) {
+  var ds = server.dataSources.postgresDB;
+  ds.automigrate(null, function(err) {
     if (err) throw err;
-  })
-}
+  });
+};
