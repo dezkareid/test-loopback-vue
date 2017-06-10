a0e3b1d82f7470e6d8953222dbce87f1b58adf7c
DezkaReid <elmaildeldezkareid@gmail.com>
2017-06-10 08:04:02 -0500

    Added submit method to submit form

--- skydrop-landing/src/components/ContactForm.vue
+++ skydrop-landing/src/components/ContactForm.vue
@@ -5,53 +5,67 @@
         <h2 class="center">Contacto</h2>
         <form @submit.prevent="submit" action="" method="post">
           <div class="input-field">
-            <v-text-input 
+            <input type="text"
               name="name" id="name" v-model="message.name"
-              placeholder="Joel Humberto Gómez Paredes" required>
-            </v-text-input>
+              data-vv-as="nombre" v-validate data-vv-rules="required"
+              placeholder="Joel Humberto Gómez Paredes" />
             <label for="name">Nombre</label>
+            <span class="invalid">{{errors.first('name')}}</span>
           </div>
           <div class="input-field">
-            <v-text-input type="tel" 
+            <input type="tel" 
               name="phone" id="phone" v-model="message.phone"
-              placeholder="1234567890" required>
-            </v-text-input>
+              data-vv-as="celular" v-validate
+              data-vv-rules="required|digits:10"
+              placeholder="1234567890"/>
             <label for="phone">Celular (10 dígitos)</label>
+            <span class="invalid">{{errors.first('phone')}}</span>
           </div>
           <div class="input-field">
-            <v-text-input type="email" 
+            <input type="email" 
               name="email" id="email" v-model="message.email"
-              placeholder="elmaildeldezkareid@gmail.com" required>
-            </v-text-input>
+              v-validate data-vv-rules="required|email"
+              placeholder="elmaildeldezkareid@gmail.com"/>
             <label for="email">Email</label>
+            <span class="invalid">{{errors.first('email')}}</span>
           </div>
           <div class="input-field">
-            <v-text-input 
+            <input type="text" 
               id="company" name="company" v-model="message.company"
-              placeholder="Nombre de la Empresa o Negocio">
-            </v-text-input>
-            <label for="name">Empresa (opcional)</label>
+              data-vv-as="empresa" v-validate
+              data-vv-rules="regex:^([A-Za-z\d\-_\-.\s]+)$"
+              placeholder="Nombre de la Empresa o Negocio"/>
+            <label for="company">Empresa (opcional)</label>
+            <span class="invalid">{{errors.first('company')}}</span>
           </div>
           <div class="input-field">
-            <v-select name="category" id="category" :items="categories"
+            <v-select name="category" id="category"
+            v-model="message.category" :items="categories"
             select-text="Seleccionar tipo de empresa">
             </v-select>
-            <label for="select">Tipo de empresa (opcional)</label>
+            <label for="category">Tipo de empresa (opcional)</label>
           </div>
           <div class="input-field">
             <v-select name="topic" id="topic"
             v-model="message.topic" :items="topics"
             select-text="Seleccionar asunto">
             </v-select>
-            <label for="select">Asunto</label>
+            <label for="topic">Asunto</label>
+            <span class="invalid">{{errors.first('topic')}}</span>
           </div>
           <div class="input-field">
-            <v-text-area name="comment" id="comment" v-model="message.comment" required>
-            </v-text-area>
+            <textarea class="materialize-textarea" data-length="120"
+            name="comment" id="comment" v-model="message.comment"
+            data-vv-as="comentario" v-validate data-vv-rules="required">
+            </textarea>
             <label for="comment">Dudas, comentarios o reporte</label>
+            <span class="invalid">{{errors.first('comment')}}</span>
           </div>
           <div class="center">
-            <v-btn class="send-form-button" type="submit">Enviar</v-btn>
+            <v-btn class="send-form-button" 
+              :disabled="buttonDisabled" type="submit">
+              Enviar
+            </v-btn>
           </div>
         </form>
       </v-grid>
@@ -60,23 +74,81 @@
 </template>
 
 <script>
+  import requester from '../requester'
+
   export default {
     name: 'contactform',
     data () {
       return {
         categories: [
-          { text: 'Restaurante' },
-          { text: 'Local' }
+          { id: 0, text: 'Restaurante' },
+          { id: 1, text: 'Local' }
         ],
         message: {},
+        buttonDisabled: false,
         topics: [
-          { text: 'Quiero más información' }
+          { id: 0, text: 'Quiero más información' },
+          { id: 1, text: 'Acerca de mi envío' },
+          { id: 2, text: 'Soporte técnico' }
         ]
       }
     },
     methods: {
       submit: function () {
-        console.log(this.message)
+        let message = this.message
+        let messageData = {
+          name: message.name,
+          phone: message.phone,
+          email: message.email,
+          comment: message.comment
+        }
+        if (!message.topic) {
+          this.$swal(
+            'Debes elegir un asunto',
+            '',
+            'error'
+          )
+          return false
+        } else {
+          messageData.topic = this.topics[+message.topic].text
+        }
+        if (message.company) {
+          if (!message.category) {
+            this.$swal(
+              'Debes elegir una categoria',
+              '',
+              'error'
+            )
+            return false
+          } else {
+            messageData.category = this.categories[+message.category].text
+          }
+        }
+
+        this.$validator.validateAll()
+          .then(() => {
+            this.$Progress.start()
+            this.buttonDisabled = true
+            return requester.save('Messages', messageData)
+          })
+          .then(() => {
+            this.$Progress.finish()
+            this.buttonDisabled = false
+            this.$swal(
+              'Mensaje enviado',
+              'Te atenderemos a la brevedad',
+              'success'
+            )
+          })
+          .catch(() => {
+            this.$Progress.fail()
+            this.buttonDisabled = false
+            this.$swal(
+              'Ocurrio un error',
+              'Intenta nuevamente',
+              'error'
+            )
+          })
       }
     }
   }
@@ -86,6 +158,10 @@
   .container .row {
     padding-top: 9rem;
     padding-bottom: 9rem;
+  }
+
+  .invalid {
+    color: #F44336;
   }
 
   .send-form-button {
