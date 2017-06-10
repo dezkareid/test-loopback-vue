737f3b5e5fd1373f902a076f300212f8f173ec0a
DezkaReid <elmaildeldezkareid@gmail.com>
2017-06-10 07:40:18 -0500

    test para mensajes

--- skydrop-messages/test/models/message.test.js
+++ skydrop-messages/test/models/message.test.js
@@ -1,20 +1,20 @@
-var app = require('../../server/server')
-var expect = require('chai').expect
+'use strict';
+
+var app = require('../../server/server');
+var expect = require('chai').expect;
 
 describe('Model/Message', function(done) {
   describe('#validateModel', function() {
-
     it('expect be created with valid data', function(done) {
       let data = {
-        'name' : 'Joel Humberto Gómez Paredes',
-        'phone' : 2281301632,
-        'email' : 'elmaildeldezkareid@gmail.com',
-        'topic' : 'Quiero más información',
-        'comment' : 'muchas cosas wuuuuuu'
+        'name': 'Joel Humberto Gómez Paredes',
+        'phone': 2281301632,
+        'email': 'elmaildeldezkareid@gmail.com',
+        'topic': 'Quiero más información',
+        'comment': 'muchas cosas wuuuuuu',
       };
 
       app.models.Message.create(data, function(err, res) {
-        console.log(err)
         expect(err).to.be.null;
         done();
       });
@@ -22,9 +22,10 @@ describe('Model/Message', function(done) {
 
     it('expect not be created without name property', function(done) {
       let data = {
-        'phone' : 2281301632,
-        'email' : 'elmaildeldezkareid@gmail.com',
-        'comment' : 'muchas cosas wuuuuuu'
+        'phone': 2281301632,
+        'email': 'elmaildeldezkareid@gmail.com',
+        'topic': 'Quiero más información',
+        'comment': 'muchas cosas wuuuuuu',
       };
 
       app.models.Message.create(data, function(err, res) {
@@ -33,4 +34,4 @@ describe('Model/Message', function(done) {
       });
     });
   });
-})
+});
