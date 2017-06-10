fe46462f56d9b66a69f30636abce41dfc7d704c1
DezkaReid <elmaildeldezkareid@gmail.com>
2017-06-10 08:00:16 -0500

    Added locale to veeValidate, sweetalert and progress bar

--- skydrop-landing/src/main.js
+++ skydrop-landing/src/main.js
@@ -4,9 +4,24 @@ import Vue from 'vue'
 import App from './App'
 import Materials from 'vue-materials'
 import VeeValidate from 'vee-validate'
+import es from 'vee-validate/dist/locale/es'
+import VueSweetAlert from 'vue-sweetalert'
+import VueProgressBar from 'vue-progressbar'
 
 Vue.use(Materials)
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  locale: 'es',
+  dictionary: {
+    es
+  }
+})
+Vue.use(VueSweetAlert)
+Vue.use(VueProgressBar, {
+  color: 'rgb(143, 255, 199)',
+  failedColor: 'red',
+  height: '2px'
+})
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
